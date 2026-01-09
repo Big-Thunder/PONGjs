e3424c890aa0d6ee1e77f05eb375f1f508b784e1
Shubham <elctro2040@gmail.com>
2026-01-10 01:13:43 +0530

    even better collisions

--- script.js
+++ script.js
@@ -5,25 +5,20 @@ let leftPlayerScore = 0;
 let rightPlayerScore = 0;
 
 function getRandomInt(min, max) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
-  let res = Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
-  console.log(res);
-  return res;
+    return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function initGameState(){
+function initGameState() {
     ball.setPos(WIDTH / 2, HEIGHT / 2, false);
 }
 
-function drawText(){
+function drawText() {
     let tSize = 150;
-
     textSize(tSize);
     textAlign(CENTER, CENTER);
     fill(100);
-    text(leftPlayerScore, (WIDTH/6), HEIGHT/2);
-    text(rightPlayerScore, WIDTH * (5/6), HEIGHT/2);
+    text(leftPlayerScore, WIDTH / 6, HEIGHT / 2);
+    text(rightPlayerScore, WIDTH * 5 / 6, HEIGHT / 2);
 }
 
 function setup() {
@@ -34,20 +29,16 @@ const pedalParamsLeft = {
     x: 10, y: 10,
     w: 5, h: 60,
 
-    getCollisionRect(){
-        return {left: this.x, right: this.x + this.w, top: this.y, bottom: this.y + this.h};
+    getCollisionRect() {
+        return {
+            left: this.x, right: this.x + this.w, top: this.y, bottom: this.y + this.h
+        };
     },
 
-    move(y){
+    move(y) {
         this.y += y;
-
-        if(this.y < 0){
-            this.y = 0;
-        }
-
-        if(this.y > HEIGHT - this.h){
-            this.y = HEIGHT - this.h;
-        }
+        if (this.y < 0) this.y = 0;
+        if (this.y > HEIGHT - this.h) this.y = HEIGHT - this.h;
     }
 };
 
@@ -55,144 +46,126 @@ const pedalParamsRight = {
     x: 770, y: 10,
     w: 5, h: 60,
 
-    getCollisionRect(){
-        return {left: this.x, right: this.x + this.w, top: this.y, bottom: this.y + this.h};
-
+    getCollisionRect() {
+        return {
+            left: this.x, right: this.x + this.w, top: this.y, bottom: this.y + this.h
+        };
     },
 
-    move(y){
+    move(y) {
         this.y += y;
-
-        if(this.y < 0){
-            this.y = 0;
-        }
-
-        if(this.y > HEIGHT - this.h){
-            this.y = HEIGHT - this.h;
-        }
+        if (this.y < 0) this.y = 0;
+        if (this.y > HEIGHT - this.h) this.y = HEIGHT - this.h;
     }
 };
 
 const ball = {
-    x: WIDTH/2, y: HEIGHT/2,
+    x: WIDTH / 2, y: HEIGHT / 2,
     w: 15, h: 15,
-    moveX: 1, moveY:1, speed: 5,
+    moveX: 1, moveY: 0,
+    speed: 5,
     canMove: false,
 
-    getCollisionRect(){
-        return {left: this.x, right: this.x + this.w, top: this.y, bottom: this.y + this.h};
+    getCollisionRect() {
+        return {
+            left: this.x, right: this.x + this.w, top: this.y, bottom: this.y + this.h
+        };
     },
 
-    setCanMove(_canMove){
-        this.canMove = _canMove;
-        let dir = getRandomInt(0, 2);
-
-        this.moveX = dir === 0 ? 1 : -1;
+    setCanMove(canMove) {
+        this.canMove = canMove;
+        this.moveX = getRandomInt(0, 2) === 0 ? 1 : -1;
+        this.moveY = getRandomInt(0, 2) === 0 ? 0.5 : -0.5;
     },
 
-    setPos(posX, posY, resetMove){
-        this.x = posX;
-        this.y = posY;
-        this.canMove = resetMove;
+    setPos(x, y, canMove) {
+        this.x = x;
+        this.y = y;
+        this.canMove = canMove;
     },
 
-    move(){
-        if(!this.canMove){
+    move() {
+        if (!this.canMove) 
             return;
-        }
 
         this.x += this.moveX * this.speed;
         this.y += this.moveY * this.speed;
 
-        //wall collisions
-        if(this.x < 0){
+        if (this.x < 0) {
             rightPlayerScore++;
             initGameState();
         }
 
-        if(this.x > WIDTH - this.w){
+        if (this.x > WIDTH - this.w) {
             leftPlayerScore++;
             initGameState();
         }
 
-        if(this.y < 0 || this.y > HEIGHT - this.h){
+        if (this.y < 0 || this.y > HEIGHT - this.h) {
             this.moveY *= -1;
         }
 
-        //pedal collisions
-        // if(this.x < pedalParamsLeft.x + pedalParamsLeft.w && this.y < pedalParamsLeft.y + pedalParamsLeft.h && this.y > pedalParamsLeft.y){
-        //     this.moveX *= -1;
-        // }
+        computeCollision(this, pedalParamsLeft, "left");
+        computeCollision(this, pedalParamsRight, "right");
+    }
+};
 
-        if(this.getCollisionRect().top < pedalParamsLeft.getCollisionRect().bottom && this.getCollisionRect().bottom > pedalParamsLeft.getCollisionRect().top && this.getCollisionRect().left < pedalParamsLeft.getCollisionRect().right && this.getCollisionRect().right > pedalParamsLeft.getCollisionRect().left){
-            if(this.moveX > 0){
-                return;
-            }
+function computeCollision(ball, pedal, side) {
+    const b = ball.getCollisionRect();
+    const p = pedal.getCollisionRect();
 
-            this.moveX *= -1;
-        }
+    if (b.top < p.bottom && b.bottom > p.top && b.left < p.right && b.right > p.left) {
+        if (side === "left" && ball.moveX > 0) 
+            return;
+        if (side === "right" && ball.moveX < 0) 
+            return;
 
-        // if(this.x > pedalParamsRight.x - pedalParamsRight.w && this.y < pedalParamsRight.y + pedalParamsRight.h && this.y > pedalParamsRight.y){
-        //     this.moveX *= -1;
-        // }
+        ball.moveX *= -1;
 
-        if(this.getCollisionRect().top < pedalParamsRight.getCollisionRect().bottom && this.getCollisionRect().bottom > pedalParamsRight.getCollisionRect().top && this.getCollisionRect().left < pedalParamsRight.getCollisionRect().right && this.getCollisionRect().right > pedalParamsRight.getCollisionRect().left){
-            if(this.moveX < 0){
-                return;
-            }
+        const pedalCenterY = pedal.y + pedal.h / 2;
+        const ballCenterY = ball.y + ball.h / 2;
 
-            this.moveX *= -1;
+        let offset = (ballCenterY - pedalCenterY) / (pedal.h / 2);
+
+        if (offset > 1) offset = 1;
+        if (offset < -1) offset = -1;
+
+        ball.moveY = offset;
+
+        if (Math.abs(ball.moveY) < 0.15) {
+            ball.moveY = ball.moveY < 0 ? -0.15 : 0.15;
         }
     }
 }
 
 function draw() {
-    if(keyIsDown(UP_ARROW) === true){
-        if(!ball.canMove){
-            ball.setCanMove(true);
-        }
-
+    if (keyIsDown(UP_ARROW)) {
+        if (!ball.canMove) ball.setCanMove(true);
         pedalParamsRight.move(-10);
     }
-    
-    if(keyIsDown(DOWN_ARROW) === true){
-        if(!ball.canMove){
-            ball.setCanMove(true);
-        }
 
+    if (keyIsDown(DOWN_ARROW)) {
+        if (!ball.canMove) ball.setCanMove(true);
         pedalParamsRight.move(10);
     }
-    
-    if(keyIsDown(87) === true){
-        if(!ball.canMove){
-            ball.setCanMove(true);
-        }
 
+    if (keyIsDown(87)) {
+        if (!ball.canMove) ball.setCanMove(true);
         pedalParamsLeft.move(-10);
     }
-    
-    if(keyIsDown(83) === true){
-        if(!ball.canMove){
-            ball.setCanMove(true);
-        }
 
+    if (keyIsDown(83)) {
+        if (!ball.canMove) ball.setCanMove(true);
         pedalParamsLeft.move(10);
     }
 
-    // if(keyIsDown(82) === true){
-    //     initGameState();
-    // }
-
-    
-    background(20, 20, 20);
-    
+    background(20);
     drawText();
     fill(255);
 
     rect(pedalParamsLeft.x, pedalParamsLeft.y, pedalParamsLeft.w, pedalParamsLeft.h);
     rect(pedalParamsRight.x, pedalParamsRight.y, pedalParamsRight.w, pedalParamsRight.h);
     rect(ball.x, ball.y, ball.w, ball.h);
-    
+
     ball.move();
 }
-
